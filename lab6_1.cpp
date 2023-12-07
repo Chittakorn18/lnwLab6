b0947f98c584e974d5850389c5642f91b71a9a59
CN <chittakorn_ph@cmu.ac.th>
2023-12-08 00:05:52 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,35 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+int main() {
+    const int max = 100;
+    int nums[max];
+
+    int input, countEven = 0, countOdd = 0;
+    int i = 0;
+
+    while (i < max) {
+        cout << "Enter an integer: ";
+        cin >> input;
+
+        if (input != 0) {
+            nums[i] = input;
+            i++;
+        } else {
+            break;
+        }
+    }
+
+    for (int z = 0; z < i; z++) {
+        if (nums[z] % 2 == 0) {
+            countEven++;
+        } else {
+            countOdd++;
+        }
+    }
+
+    cout << "#Even numbers = " << countEven << endl;
+    cout << "#Odd numbers = " << countOdd << endl;
+
     return 0;
 }
